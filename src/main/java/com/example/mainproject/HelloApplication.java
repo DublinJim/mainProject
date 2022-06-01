823782f7ab822a279c418e5ad034309909a7a2b7
James Keogh <jkeogh@adga.ca>
2022-06-01 15:39:45 -0400

    made a change

--- src/main/java/com/example/mainproject/HelloApplication.java
+++ src/main/java/com/example/mainproject/HelloApplication.java
@@ -11,6 +11,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        System.out.println("I hate this");
         FXMLLoader fxmlLoader = new FXMLLoader(
             HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
