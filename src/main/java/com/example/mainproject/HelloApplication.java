0ea2411d374163cfe6ad41a565f7deefda843f71
James Keogh <jkeogh@adga.ca>
2022-06-01 15:49:56 -0400

    adds Id

--- src/main/java/com/example/mainproject/HelloApplication.java
+++ src/main/java/com/example/mainproject/HelloApplication.java
@@ -12,6 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         System.out.println("I hate this");
+        System.out.println("This is the laptop");
         FXMLLoader fxmlLoader = new FXMLLoader(
             HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
