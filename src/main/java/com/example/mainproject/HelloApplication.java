4597adc903cd12c9b10d5d6f1ffb31f8be294210
James Keogh <jkeogh@adga.ca>
2022-06-02 08:56:31 -0400

    premerge

--- src/main/java/com/example/mainproject/HelloApplication.java
+++ src/main/java/com/example/mainproject/HelloApplication.java
@@ -1,27 +1,34 @@
 package com.example.mainproject;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
-
-    @Override
-    public void start(Stage stage) throws IOException {
-    System.out.println("added code");
-    System.out.println("push 1");
-        FXMLLoader fxmlLoader = new FXMLLoader(
-            HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Laptop");
-        stage.setScene(scene);
-        stage.show();
-    }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        System.out.println("this is my pc ");
+
+        System.out.println("added code");
+
+        System.out.println("push 1");
+        FXMLLoader fxmlLoader = new FXMLLoader(
+            HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        stage.setTitle("Desktop pc for sure");
+
+        stage.setTitle("Laptop");
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
