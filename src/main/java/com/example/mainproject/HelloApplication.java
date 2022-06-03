c58a1a0aad35dbb7333b8b2d2ad89f500bff7082
James Keogh <jkeogh@adga.ca>
2022-06-03 09:21:32 -0400

    added a line

--- src/main/java/com/example/mainproject/HelloApplication.java
+++ src/main/java/com/example/mainproject/HelloApplication.java
@@ -1,26 +1,35 @@
 package com.example.mainproject;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
 
     @Override
     public void start(Stage stage) throws IOException {
 
+        System.out.println("June 3 ");
+
+        System.out.println("added code");
+        System.out.println("added more code");
+
+        System.out.println("push 1");
         FXMLLoader fxmlLoader = new FXMLLoader(
             HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Desktop pc");
+
+        stage.setTitle("Desktop pc for sure");
+
+        stage.setTitle("Laptop");
+
         stage.setScene(scene);
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
     }
 }
