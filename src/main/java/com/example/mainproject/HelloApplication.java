f74b2701a49a4374fa5c742086257ebb45fee800
James Keogh <jkeogh@adga.ca>
2022-06-03 11:17:25 -0400

    line 27

--- src/main/java/com/example/mainproject/HelloApplication.java
+++ src/main/java/com/example/mainproject/HelloApplication.java
@@ -24,7 +24,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(
             HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-
+    System.out.println( "line 27 going form desktop to git");
         stage.setTitle("Desktop pc for sure");
         System.out.println("added more code");
         stage.setTitle("Laptop");
